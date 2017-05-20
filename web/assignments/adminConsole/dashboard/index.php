97240125bf13a63dcf9833cd7d06b5eb26a55a84
Garrett <garbar94@gmail.com>
2017-05-20 16:08:17 -0600

    Fixed the layout of the charts on dashboard

--- web/assignments/adminConsole/dashboard/index.php
+++ web/assignments/adminConsole/dashboard/index.php
@@ -3,7 +3,7 @@
 
 <head>
     <meta charset="utf-8" />
-    <title>Door Step Dates - Admin Console</title>
+    <title>Door Step Dates - Dashboard</title>
     <link href="/materialize/css/materialize.css" type="text/css" rel="stylesheet">
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet">
@@ -12,24 +12,18 @@
     <script src="../../adminConsole/js/charts.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.5.0/Chart.min.js"></script>
-    <script src="http://cdnjs.cloudflare.com/ajax/libs/gsap/1.18.0/TweenMax.min.js"></script>
     <script src="../../../materialize/js/materialize.js"></script>
     <script src="../../adminConsole/js/admin.js"></script>
-
-
 
 </head>
 
 <body onload="saveSession(); loaded();">
     <h3 class="center">Door Step Dates: Dashboard</h3>
     <?php
-            include( $_SERVER['DOCUMENT_ROOT'] . '/assignments/adminConsole/nav.php' ); 
-        ?>
-        <p>&nbsp;</p>
+        include( $_SERVER['DOCUMENT_ROOT'] . '/assignments/adminConsole/nav.php' ); 
+    ?>
         <div class="row">
-            <div class="col s2">
-            </div>
-            <div class="col s2">
+            <div class="col s3">
                 <div class="header-label">
                     <p>Active Users</p>
                     <div class="userCounter-container">
@@ -37,17 +31,17 @@
                     </div>
                 </div>
             </div>
-            <div class="col s2">
+            <div class="col s3">
                 <div id="chart">
                     <canvas id="genderChart"></canvas>
                 </div>
             </div>
-            <div class="col s2">
+            <div class="col s3">
                 <div id="chart">
                     <canvas id="genderActiveChart"></canvas>
                 </div>
             </div>
-            <div class="col s2">
+            <div class="col s3">
                 <div class="header-label">
                     <p>Active Paying Users</p>
                     <div class="userCounter-container">
@@ -55,13 +49,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col s2">
-            </div>
-        </div>
-        <div class="row">
-            <div class="col s2"></div>
-            <div class="col s2"></div>
-            <div class="col s2">
+            <div class="col s3 counter-divider"></div>
+            <div class="col s3 counter-divider">
                 <div class="header-label">
                     <p>Total Stock</p>
                     <div class="userCounter-container">
@@ -69,7 +58,7 @@
                     </div>
                 </div>
             </div>
-            <div class="col s2">
+            <div class="col s3 counter-divider">
                 <div class="header-label">
                     <p>Inventory Lent Out</p>
                     <div class="userCounter-container">
@@ -77,10 +66,8 @@
                     </div>
                 </div>
             </div>
-            <div class="col s2"></div>
-            <div class="col s2"></div>
+            <div class="col s3 counter-divider"></div>
         </div>
-
 
 </body>
 
